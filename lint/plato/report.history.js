f26e81c089590357e59d9f504968518334e242fa
uupaa <uupaa.js@gmail.com>
2014-06-16 01:33:10 +0900

    Valid to

---
+++ lint/plato/report.history.js
@@ -0,0 +1,46 @@
+__history = [
+  {
+    "date": "Thu, 29 May 2014 05:50:19 GMT",
+    "total": {
+      "sloc": 124,
+      "maintainability": 65.07969677329385
+    },
+    "average": {
+      "sloc": 124,
+      "maintainability": "65.08"
+    }
+  },
+  {
+    "date": "Thu, 29 May 2014 05:51:10 GMT",
+    "total": {
+      "sloc": 124,
+      "maintainability": 65.07969677329385
+    },
+    "average": {
+      "sloc": 124,
+      "maintainability": "65.08"
+    }
+  },
+  {
+    "date": "Thu, 29 May 2014 05:52:48 GMT",
+    "total": {
+      "sloc": 124,
+      "maintainability": 65.07969677329385
+    },
+    "average": {
+      "sloc": 124,
+      "maintainability": "65.08"
+    }
+  },
+  {
+    "date": "Sun, 15 Jun 2014 16:32:25 GMT",
+    "total": {
+      "sloc": 130,
+      "maintainability": 66.78604073123273
+    },
+    "average": {
+      "sloc": 130,
+      "maintainability": "66.79"
+    }
+  }
+]
